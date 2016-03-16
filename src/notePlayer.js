c05ac0158d13804c90490ee2388d2dbc33277a98
laopunk <contact@laopunk.com>
2016-03-16 17:37:55 +0000

    adding Attack and Release to notePlayer

--- src/notePlayer.js
+++ src/notePlayer.js
@@ -5,11 +5,11 @@ var _ = require('lodash');
 * @author David B - laopunk 
 * @class notePlayer
 * @classdesc A musical note that can be played in a browser. Plays for however long it has been defined to.
-* @param {Object} obj                         - properties needed to instanciate the class
+* @param {Object} obj                       - properties needed to instanciate the class
 * @param {Object} obj.keynb                 - corresponding number on a piano keyboard
-* @param {Object} obj.freq                     - sound frequency, in Hertz
-* @param {Object} obj.octave                 - musical octave the note belongs to [1-8]
-* @param {Object} obj.name                     - Full name of the note
+* @param {Object} obj.freq                  - sound frequency, in Hertz
+* @param {Object} obj.octave                - musical octave the note belongs to [1-8]
+* @param {Object} obj.name                  - Full name of the note
 * @property {number}  pianoKeyNb            - corresponding number on a piano keyboard
 * @property {number}  frequency             - sound frequency, in Hertz
 * @property {number}  octave                - musical octave the note belongs to [1-8]
@@ -17,6 +17,8 @@ var _ = require('lodash');
 * @property {number}  duration              - length of time the sound has to be played, in seconds
 * @property {number}  volume                - volume [0-1]
 * @property {Boolean}  verbose              - verbose mode (true/false)
+* @property {number}  attack                - time to ramp up to the desired volume [0-1]
+* @property {number}  release               - time to ramp up from the desired volume to silence [0-1]
 * @property {Object} audioContext           - WebAudio audioContext
 * @property {Object} destinationNode        - WebAudio destinationNode
 */
@@ -28,8 +30,10 @@ function notePlayer(obj){
         this.octave = obj.name.substr(-1,1)
         this.name = obj.name
         this.duration = _.random(0.5,3,true)
-        this.volume = 1;
+        this.volume = 1
         this.verbose = false
+        this.attack = 0.3
+        this.release = 0.1
 
         //audio API
         this.audioContext = (obj.audioContext === void 0) ? new (window.AudioContext || window.webkitAudioContext)() : obj.audioContext;
@@ -184,7 +188,12 @@ notePlayer.prototype.play = function(callback) {
     var oscillator = this.audioContext.createOscillator()
     oscillator.frequency.value = this.frequency
     var gainNode = this.audioContext.createGain()
-    gainNode.gain.value = this.volume
+    if (this.attack) {
+      gainNode.gain.linearRampToValueAtTime(this.volume, this.audioContext.currentTime + this.attack);
+      gainNode.gain.setTargetAtTime(0, this.audioContext.currentTime + this.attack, this.release || this.attack);
+    } else {
+      gainNode.gain.value = this.volume
+    }
 
     //Connections
     oscillator.connect(gainNode)
@@ -254,5 +263,24 @@ notePlayer.prototype.setVerbose = function(v) {
     this.verbose = (v === void 0 || v === true) ? true : false;
 };
 
+/**
+ * @function setAttack
+ * @description Updates value of the note's attack
+ * @example setAttack(0.3)
+ * @param {Number} n         - [0-1] the closer to 0 the longer the attack
+ */
+notePlayer.prototype.setAttack = function(n) {
+    this.attack = (n === void 0 || n % 1 === 0 ) ? this.attack : n;
+};
+
+/**
+ * @function setRelease
+ * @description Updates value of the note's release
+ * @example setRelease(0.3)
+ * @param {Number} n         - [0-1] the closer to 0 the longer the release
+ */
+notePlayer.prototype.setRelease = function(n) {
+    this.release = (n === void 0 || n % 1 === 0 ) ? this.release : n;
+};
 
 module.exports = notePlayer
